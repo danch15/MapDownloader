408ac50b112d36a72d784a7effb31d396380eb46
Danch Lian <danch@live.cn>
2021-01-25 18:34:56 +0800

    GMapProvidersExt 移除WinForm的引用

--- GMapProvidersExt/AMap/AMapProvider.cs
+++ GMapProvidersExt/AMap/AMapProvider.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Web;
 using GMap.NET.MapProviders;
 using GMap.NET.Projections;
-using GMap.NET.WindowsForms;
+//using GMap.NET.WindowsForms;
 using NetUtil;
 using Newtonsoft.Json.Linq;
 using log4net;
@@ -300,67 +300,67 @@ namespace GMapProvidersExt.AMap
 
         #region RoutingProvider Members
 
-        public GMapRoute GetRoute(PointLatLng start, PointLatLng end,string cityName)
-        {
-            List<PointLatLng> points = new List<PointLatLng>();
-            string origin = string.Format("{0},{1}", start.Lng, start.Lat);
-            string destination = string.Format("{0},{1}", end.Lng, end.Lat);
-            string city = HttpUtility.UrlEncode(cityName);
-            string url = string.Format("http://restapi.amap.com/v3/direction/transit/integrated?origin={0}&destination={1}&city={2}&output=json&key={3}", origin, destination, city, KEY);
-            string result = HttpUtil.GetData(url);
-            if (!string.IsNullOrEmpty(result))
-            {
-                JObject resJosn = JObject.Parse(result);
-                string isOk = resJosn["info"].ToString();
-                if (isOk == "OK")
-                {
-                    JObject route = (JObject)resJosn["route"];
-                    string ori = route["origin"].ToString();
-                    string des = route["destination"].ToString();
-                    string distance = route["distance"].ToString();
-                    JObject transit = (JObject)route["transits"][0];//公交换乘方案
-                    JArray segments = (JArray)transit["segments"]; //换乘路段列表
-                    JObject firstSeg = (JObject)segments[0];
-                    JObject walk = (JObject)firstSeg["walking"];
-                    if (walk != null)
-                    {
-                        JArray steps = (JArray)walk["steps"];
-                        foreach (JObject step in steps)
-                        {
-                            string polyline = step["polyline"].ToString();
-                            string[] pointsString = polyline.Split(';');
-                            foreach (string pStr in pointsString)
-                            {
-                                string[] pointStr = pStr.Split(',');
-                                PointLatLng p = new PointLatLng(double.Parse(pointStr[1]), double.Parse(pointStr[0]));
-                                points.Add(p);
-                            }
-                        }
-                    }
-                    JObject bus = (JObject)firstSeg["bus"];
-                    if (bus != null)
-                    {
-                        JArray steps = (JArray)bus["buslines"];
-                        //foreach (JObject step in steps)
-                        if (steps != null && steps.Count>0)
-                        {
-                            JObject step = (JObject)steps[0];
-                            string polyline = step["polyline"].ToString();
-                            string[] pointsStr = polyline.Split(';');
-                            foreach (string pStr in pointsStr)
-                            {
-                                string[] pointStr = pStr.Split(',');
-                                PointLatLng p = new PointLatLng(double.Parse(pointStr[1]), double.Parse(pointStr[0]));
-                                points.Add(p);
-                            }
-                        }
-                    }
-                }
-            }
+        //public GMapRoute GetRoute(PointLatLng start, PointLatLng end,string cityName)
+        //{
+        //    List<PointLatLng> points = new List<PointLatLng>();
+        //    string origin = string.Format("{0},{1}", start.Lng, start.Lat);
+        //    string destination = string.Format("{0},{1}", end.Lng, end.Lat);
+        //    string city = HttpUtility.UrlEncode(cityName);
+        //    string url = string.Format("http://restapi.amap.com/v3/direction/transit/integrated?origin={0}&destination={1}&city={2}&output=json&key={3}", origin, destination, city, KEY);
+        //    string result = HttpUtil.GetData(url);
+        //    if (!string.IsNullOrEmpty(result))
+        //    {
+        //        JObject resJosn = JObject.Parse(result);
+        //        string isOk = resJosn["info"].ToString();
+        //        if (isOk == "OK")
+        //        {
+        //            JObject route = (JObject)resJosn["route"];
+        //            string ori = route["origin"].ToString();
+        //            string des = route["destination"].ToString();
+        //            string distance = route["distance"].ToString();
+        //            JObject transit = (JObject)route["transits"][0];//公交换乘方案
+        //            JArray segments = (JArray)transit["segments"]; //换乘路段列表
+        //            JObject firstSeg = (JObject)segments[0];
+        //            JObject walk = (JObject)firstSeg["walking"];
+        //            if (walk != null)
+        //            {
+        //                JArray steps = (JArray)walk["steps"];
+        //                foreach (JObject step in steps)
+        //                {
+        //                    string polyline = step["polyline"].ToString();
+        //                    string[] pointsString = polyline.Split(';');
+        //                    foreach (string pStr in pointsString)
+        //                    {
+        //                        string[] pointStr = pStr.Split(',');
+        //                        PointLatLng p = new PointLatLng(double.Parse(pointStr[1]), double.Parse(pointStr[0]));
+        //                        points.Add(p);
+        //                    }
+        //                }
+        //            }
+        //            JObject bus = (JObject)firstSeg["bus"];
+        //            if (bus != null)
+        //            {
+        //                JArray steps = (JArray)bus["buslines"];
+        //                //foreach (JObject step in steps)
+        //                if (steps != null && steps.Count>0)
+        //                {
+        //                    JObject step = (JObject)steps[0];
+        //                    string polyline = step["polyline"].ToString();
+        //                    string[] pointsStr = polyline.Split(';');
+        //                    foreach (string pStr in pointsStr)
+        //                    {
+        //                        string[] pointStr = pStr.Split(',');
+        //                        PointLatLng p = new PointLatLng(double.Parse(pointStr[1]), double.Parse(pointStr[0]));
+        //                        points.Add(p);
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
             
-            GMapRoute mapRoute = points != null ? new GMapRoute(points, "") : null;
-            return mapRoute;
-        }
+        //    GMapRoute mapRoute = points != null ? new GMapRoute(points, "") : null;
+        //    return mapRoute;
+        //}
 
         public MapRoute GetRoute(PointLatLng start, PointLatLng end, bool avoidHighways, bool walkingMode, int Zoom)
         {
